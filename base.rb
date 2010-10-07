2328a05903570f69f057a07c66d9da8c9229ce84
Stephen Aument <step@stephenaument.com>
2010-10-07 15:55:02 -0700

    Add header comments to the file.
    - Add header comments.
    - tweak the first line of the puts.

--- base.rb
+++ base.rb
@@ -1,3 +1,21 @@
+# Application Generator Template
+# Modifies a Rails app to include my personal preferences.
+# Usage: rails new app_name -m http://github.com/stephenaument/rails-templates/raw/master/base.rb
+#
+# Thanks to ryanb: http://github.com/ryanb and to  from whom this repository is forked and
+# fortuity whose rails3-mongoid-devise template 
+# http://github.com/fortuity/rails3-mongoid-devise/raw/master//template.rb
+# was consulted, as well.
+#
+# More info: http://github.com/stephenaument/rails-templates/
+
+# If you are customizing this template, you can use any methods provided by Thor::Actions
+# http://rdoc.info/rdoc/wycats/thor/blob/f939a3e8a854616784cac1dcff04ef4f3ee5f7ff/Thor/Actions.html
+# and Rails::Generators::Actions
+# http://github.com/rails/rails/blob/master/railties/lib/rails/generators/actions.rb
+
+puts "Modifying a new Rails app to use my personal preferences..."
+
 if yes?("Do you want to use RSpec for testing?")
   plugin "rspec", :git => "git://github.com/dchelimsky/rspec.git"
   plugin "rspec-rails", :git => "git://github.com/dchelimsky/rspec-rails.git"
