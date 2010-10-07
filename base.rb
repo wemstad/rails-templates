62a1ea1151185ba3e78b59d1e85faa28cf3898b6
Stephen Aument <step@stephenaument.com>
2010-10-07 16:37:04 -0700

    Install gems for testing
    - Remove conditional. We always want to install this stuff.
    - Add shoulda to the mix.
    - Use gems instead of plugins for rspec and rspec-rails.

--- base.rb
+++ base.rb
@@ -16,11 +16,24 @@
 
 puts "Modifying a new Rails app to use my personal preferences..."
 
-if yes?("Do you want to use RSpec for testing?")
-  plugin "rspec", :git => "git://github.com/dchelimsky/rspec.git"
-  plugin "rspec-rails", :git => "git://github.com/dchelimsky/rspec-rails.git"
-  generate :rspec
-end
+#----------------------------------------------------------------------------
+# Set up Cucumber, Rspec, Factory Girl and Shoulda
+#----------------------------------------------------------------------------
+puts "Setting up Gemfile for Testing..."
+append_file 'Gemfile', "\n# Bundle gems needed for Testing\n"
+gem 'autotest', :group => [:cucumber, :test, :development]
+gem 'cucumber-rails', :version => '>=0.2.4', :group => [:cucumber, :test, :development]
+gem 'database_cleaner', :version => '>=0.4.3', :group => [:cucumber, :test, :development]
+gem 'webrat', :version => '>=0.6.0', :group => [:cucumber, :test, :development]
+gem 'shoulda', :group => [:cucumber, :test, :development]
+gem 'rspec', :version => '>= 2.0.0.beta.22', :group => [:cucumber, :test, :development]
+gem 'rspec-rails', :version => ">= 2.0.0.beta.22", :group => [:cucumber, :test, :development]
+gem 'pickle', :version => ">=0.2.1", :group => [:cucumber, :test, :development]
+gem 'factory_girl_rails', :group => [:cucumber, :test, :development]
+gem 'nokogiri', :version => ">= 1.4.0", :group => [:cucumber, :test, :development]
+
+puts "installing testing gems (takes a few minutes!)..."
+run 'bundle install'
 
 generate :nifty_layout
 
