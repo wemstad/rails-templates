2a815a3ffb053ea194fc5298e797e28720030ef4
Stephen Aument <step@stephenaument.com>
2010-10-12 13:56:17 -0700

    Add omnisocial

--- set_up_omnisocial/config/initializers/secret_token.rb
+++ set_up_omnisocial/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SetUpOmnisocial::Application.config.secret_token = '5234ac197eef5d13e916d8c38f7ee30ab979838f06c5de676b7a92901478809e13d594272ff1b67ce59b4373aef66702a5718a2d51f0c6e4e4d421dcdf38e6cc'
+SetUpOmnisocial::Application.config.secret_token = '9ceea4ddeab3ddb3ffde98b98f2c19a8615dd45829d447b79a19c74b7ac7005da52b292977a698ebdd69a07494316865377e2cf454b38a5d6688d5e3232c8edf'
--- set_up_omnisocial/db/schema.rb
+++ set_up_omnisocial/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101012204644) do
+ActiveRecord::Schema.define(:version => 20101012205609) do
 
   create_table "login_accounts", :force => true do |t|
     t.string   "type"
