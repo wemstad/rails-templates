f357b0824dc129e5dade5ab223c0add511358aee
Stephen Aument <step@stephenaument.com>
2010-10-12 14:05:42 -0700

    Add omnisocial

---
+++ multi_test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultiTest::Application.config.secret_token = 'e39cc61ef83336735a5fdc99833957b4b33869a57af7405837cf50167264aaaa307a977d771692a36ab6906a599958e48f1afa5fa5bd79ab7a0f97eb16934ce8'
