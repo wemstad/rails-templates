b31d86c62d408cfcd2d50d36420251ec4396414b
Stephen Aument <step@stephenaument.com>
2010-10-12 15:18:19 -0700

    Add _user_bar.html.haml template for omnisocial.

--- set_up_omnisocial.rb
+++ set_up_omnisocial.rb
@@ -23,6 +23,9 @@ unless @facebook_app_key.blank?
   inject_into_file 'config/initializers/omnisocial.rb', "  config.facebook '#{@facebook_app_key}', '#{@facebook_app_secret}', :scope => 'publish_stream'\n", :after => "# config.facebook 'APP_KEY', 'APP_SECRET', :scope => 'publish_stream'\n"
 end
 
+run "mkdir 'app/views/users'"
+get 'http://github.com/stephenaument/rails-templates/raw/master/templates/_user_bar_os.html.haml', 'app/views/users/_user_bar.html.haml'
+
 puts "creating database tables"
 rake 'db:migrate'
 
