bb65c001a0fefa2057f43190c92ddcd02c1c17f0
Stephen Aument <step@stephenaument.com>
2010-10-14 16:34:55 -0700

    Add modal-signin to home page for omnisocial.

--- set_up_omnisocial.rb
+++ set_up_omnisocial.rb
@@ -23,8 +23,29 @@ unless @facebook_app_key.blank?
   inject_into_file 'config/initializers/omnisocial.rb', "  config.facebook '#{@facebook_app_key}', '#{@facebook_app_secret}', :scope => 'publish_stream'\n", :after => "# config.facebook 'APP_KEY', 'APP_SECRET', :scope => 'publish_stream'\n"
 end
 
+puts "adding and configuring view files..."
 run "mkdir 'app/views/users'"
 get 'http://github.com/stephenaument/rails-templates/raw/master/templates/_user_bar_os.html.haml', 'app/views/users/_user_bar.html.haml'
+
+run "mkdir 'app/views/omnisocial"
+get 'http://github.com/stephenaument/rails-templates/raw/master/templates/os_new.html.haml', 'app/views/omnisocial/_new.html.haml'
+
+if @haml_flag
+  inject_into_file 'app/views/layouts/application.html.haml', :after => "%body\n", do
+<<-EOF
+  
+  #modal-signin.modal{:style => 'display: none;'}
+    = render :partial => 'omnisocial/new'
+EOF
+else
+    inject_into_file 'app/views/layouts/application.html.erb', :after => "<body>\n", do
+  <<-EOF
+
+    <div id='modal-signin' class='modal' style='display: none;'>
+      <%= render :partial => 'omnisocial/new' %>
+    </div>
+  EOF
+end
 
 puts "creating database tables"
 rake 'db:migrate'
